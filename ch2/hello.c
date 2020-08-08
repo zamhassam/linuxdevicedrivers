351f0635f370e48651f58b7e3b5d03c324e59a5d
Zam <zam.hassam@googlemail.com>
2020-08-08 12:46:44 +0100

    Modules compile but don't load correctly.

--- ch2/hello.c
+++ ch2/hello.c
@@ -1,17 +1,23 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 
-MODULE_LISENCE("Dual BSD/GPL");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Zam Hassam");
+MODULE_DESCRIPTION("A simple Linux driver");
+MODULE_VERSION("0.1");
 
-static int hello_init(void)
-{
-    printk(KERN_ALERT, "Hello, world\n");
-    return 0;
+static char *name = "world";
+module_param(name, charp, S_IRUGO);
+MODULE_PARM_DESC(name, "The name to display in the /var/log/kern.log");
+
+static int __init hello_init(void) {
+	printk(KERN_INFO, "Hello %s from the LKM\n", name);
+	return 0;
 }
 
-static void hello_exit(void)
-{
-    printk(KERN_ALERT, "Goodbye, cruel world\n");
+static void __exit hello_exit(void) {
+	printk(KERN_INFO, "Goodbye %s from the LKM", name);
 }
 
 module_init(hello_init);
